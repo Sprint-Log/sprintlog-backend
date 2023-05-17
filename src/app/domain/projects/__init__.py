32062fa99cb8880e358c654aafd34b07883cc704
Phyo <phyoakl@hexcode.tech>
2023-05-18 01:55:16 +0630

    fix inits added imports to precommit

--- src/app/domain/projects/__init__.py
+++ src/app/domain/projects/__init__.py
@@ -1,4 +1,4 @@
 """User Account domain logic."""
 from . import controllers, dependencies, models
-__all__ = ["models", "controllers", "dependencies"]
 
+__all__ = ["models", "controllers", "dependencies"]
