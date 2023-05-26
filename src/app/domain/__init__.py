81406749c42dabede59bcc44f88c97384a5b6b93
Phyo <phyoakl@hexcode.tech>
2023-05-26 13:34:44 +0630

    merged updated fullstack

--- src/app/domain/__init__.py
+++ src/app/domain/__init__.py
@@ -2,13 +2,25 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
+from uuid import UUID
 
+from litestar.contrib.jwt import OAuth2Login
+from litestar.contrib.repository.abc import FilterTypes
+from litestar.pagination import OffsetPagination
+from pydantic import UUID4, EmailStr
+from saq.types import QueueInfo
+
+from app.domain.accounts.models import User
 from app.lib import settings, worker
+from app.lib.service.generic import Service
 from app.lib.worker.controllers import WorkerController
 
-from . import accounts, analytics, backlogs, openapi, projects, security, system, teams, urls, web
+from . import accounts, analytics, backlogs, openapi, plugins, projects, security, system, teams, urls, web
 
 if TYPE_CHECKING:
+    from collections.abc import Mapping
+    from typing import Any
+
     from litestar.types import ControllerRouterHandler
 
 
@@ -42,6 +54,8 @@ __all__ = [
     "analytics",
     "backlogs",
     "projects",
+    "plugins",
+    "signature_namespace",
 ]
 tasks: dict[worker.Queue, list[worker.WorkerFunction]] = {
     worker.queues.get("system-tasks"): [  # type: ignore[dict-item]
@@ -59,4 +73,23 @@ scheduled_tasks: dict[worker.Queue, list[worker.CronJob]] = {
     worker.queues.get("background-tasks"): [  # type: ignore[dict-item]
         worker.CronJob(function=worker.tasks.background_worker_task, unique=True, cron="* * * * *", timeout=300),
     ],
+}
+
+
+signature_namespace: Mapping[str, Any] = {
+    "Service": Service,
+    "FilterTypes": FilterTypes,
+    "UUID4": UUID4,
+    "UUID": UUID,
+    "EmailStr": EmailStr,
+    "User": User,
+    "OAuth2Login": OAuth2Login,
+    "OffsetPagination": OffsetPagination,
+    "UserService": accounts.services.UserService,
+    "TeamService": teams.services.TeamService,
+    "TeamInvitationService": teams.services.TeamInvitationService,
+    "TeamMemberService": teams.services.TeamMemberService,
+    "Queue": worker.Queue,
+    "QueueInfo": QueueInfo,
+    "Job": worker.Job,
 }
--- src/app/domain/analytics/controllers.py
+++ src/app/domain/analytics/controllers.py
@@ -16,7 +16,7 @@ from app.lib import log
 from .schemas import NewUsersByWeek
 
 if TYPE_CHECKING:
-    from app.lib.db.base import SQLAlchemyAiosqlQueryManager
+    from app.lib.aiosql import AiosqlQueryManager
 
 
 __all__ = ["StatsController"]
@@ -31,7 +31,7 @@ class StatsController(Controller):
     tags = ["Statistics"]
     guards = [requires_active_user]
     dependencies = {
-        "analytic_queries": Provide(provides_analytic_queries, sync_to_thread=False),
+        "analytic_queries": Provide(provides_analytic_queries),
     }
 
     @get(
@@ -42,9 +42,7 @@ class StatsController(Controller):
         description="List New Users by Week.",
         cache=1000,
     )
-    async def weekly_new_users(
-        self, analytic_queries: SQLAlchemyAiosqlQueryManager
-    ) -> OffsetPagination[NewUsersByWeek]:
+    async def weekly_new_users(self, analytic_queries: AiosqlQueryManager) -> OffsetPagination[NewUsersByWeek]:
         """New Users by week."""
         results = await analytic_queries.select("users_by_week")
         return OffsetPagination[NewUsersByWeek](
