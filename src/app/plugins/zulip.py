601798109cea3d6d3a4ddddb59240777e3716ab9
Thiri Lwin <thirilwin.ucsm@gmail.com>
2023-06-21 15:16:53 +0630

    feat: integrate with zulip to send backlog info

--- src/app/plugins/zulip.py
+++ src/app/plugins/zulip.py
@@ -2,9 +2,12 @@ import logging
 from typing import Any
 from uuid import UUID
 
+import httpx
+
 from app.domain.backlogs.models import Backlog
 from app.domain.projects.models import Project
 from app.lib.plugin import BacklogPlugin, ProjectPlugin
+from app.lib.settings import server
 
 __all__ = ["ZulipBacklogPlugin"]
 logger = logging.getLogger(__name__)
@@ -13,6 +16,20 @@ logger = logging.getLogger(__name__)
 def log_info(message: str) -> None:
     return logger.info(message)
 
+
+def send_msg_to_zulip(title: str, description: str) -> dict[str:str]:
+    url: str = server.ZULIP_API_URL+server.ZULIP_SEND_MESSAGE_URL
+    auth: str = (server.ZULIP_EMAIL_ADDRESS, server.ZULIP_API_KEY)
+    log_info(url)
+    data: dict[str:str] = {
+        "type": "stream",
+        "to": server.ZULIP_STREAM_NAME,
+        "topic": title,
+        "content": description
+    }
+
+    response = httpx.post(url, auth=auth, data=data)
+    return response.json()
 
 class ZulipBacklogPlugin(BacklogPlugin):
     def __init__(self, zulip_bot: str = "pipo") -> None:
@@ -25,6 +42,11 @@ class ZulipBacklogPlugin(BacklogPlugin):
             data.plugin_meta = {"zulip_bot": self.zulip_bot}
         elif isinstance(data, dict):
             data["plugin_meta"] = {"zulip_bot": self.zulip_bot}
+        response = send_msg_to_zulip(data.title, data.description)
+        if response["result"] != "success":
+            log_info(response)
+        else:
+            log_info("successfully sent message to zulip")
         return data
 
     async def after_create(self, data: "Backlog") -> "Backlog":
