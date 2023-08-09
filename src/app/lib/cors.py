16984580fbf48982f56d00d97226b3df55dbf63e
Phyo <phyoakl@hexcode.tech>
2023-08-09 15:03:50 +0630

    fix: remove audit from sentinal migration

--- src/app/lib/cors.py
+++ src/app/lib/cors.py
@@ -2,5 +2,7 @@ from litestar.config.cors import CORSConfig
 
 from app.lib import settings
 
-config = CORSConfig(allow_origins=settings.app.BACKEND_CORS_ORIGINS, allow_credentials=True)
+config = CORSConfig(
+    allow_origins=settings.app.BACKEND_CORS_ORIGINS, allow_credentials=True, allow_methods=["*"], allow_headers=["*"]
+)
 """Default CORS config."""
