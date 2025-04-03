972a72ac30910f5fcb96ede4c485667b612ce36d
Hein Min Min Maw <heinmin2maw.it@gmail.com>
2025-04-03 12:57:47 +0700

    feat: team assigned integration, refactor dto usage with schema in ProjectController

--- src/app/domain/projects/controllers/project.py
+++ src/app/domain/projects/controllers/project.py
@@ -1,133 +1,154 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Annotated, Sequence, Any
+from typing import TYPE_CHECKING, Annotated
+from structlog import getLogger
 from uuid import UUID
 
-
-from litestar import (
-    Controller,
-    delete,
-    get,
-    post,
-    put,
-)
+from advanced_alchemy.filters import OrderBy, CollectionFilter
+from litestar import Controller, delete, get, post, put, patch
 from litestar.di import Provide
 from litestar.params import Dependency
 from litestar.status_codes import HTTP_200_OK
-from litestar.exceptions import NotFoundException
-from litestar import Response, patch
+from litestar.exceptions import NotFoundException, ValidationException
+from litestar.pagination import OffsetPagination
 
 from app.domain.accounts.guards import requires_active_user, requires_superuser
+from app.domain.teams.services import TeamService
+
 from app.domain.projects.dependencies import provide_project_service
 from app.domain.projects.services import ProjectService
-
-from uuid import UUID
-from app.lib.deps import create_filter_dependencies
-from structlog import getLogger
-from app.domain.projects.dtos import WriteDTO, ReadDTO, StatusWriteDTO
+from app.domain.projects.schemas import ProjectCreate, Project, ProjectStatusUpdate, ProjectUpdate
 from app.domain.projects import urls
 
-from advanced_alchemy.filters import OrderBy, CollectionFilter
+from app.lib.deps import create_filter_dependencies, create_service_dependencies
+
+from app.db import models as m
 
 if TYPE_CHECKING:
-    from app.db import models as m
     from advanced_alchemy.filters import FilterTypes
 
 
 logger = getLogger()
 
-if TYPE_CHECKING:
-    logger.info("Yes it's a type check")
-
 __all__ = ["ProjectController"]
 
 
 class ProjectController(Controller):
-    dto = WriteDTO
-    return_dto = ReadDTO
+
     guards = [requires_active_user]
-    tags = ["Projects API"]
-    dependencies = {"service": Provide(provide_project_service)} | create_filter_dependencies(
-        {
-            "id_filter": UUID,
-            "search": "name,email",
-            "pagination_type": "limit_offset",
-            "pagination_size": 20,
-            "created_at": True,
-            "updated_at": True,
-            "sort_field": "name",
-            "sort_order": "asc",
-        },
+    tags = ["Projects"]
+    dependencies = (
+        {"project_service": Provide(provide_project_service)}
+        | create_service_dependencies(
+            TeamService,
+            key="teams_service",
+            load=[m.Team.tags, m.Team.members],
+            filters={"id_filter": UUID},
+        )
+        | create_filter_dependencies(
+            {
+                "id_filter": UUID,
+                "search": "name,email",
+                "pagination_type": "limit_offset",
+                "pagination_size": 20,
+                "created_at": True,
+                "updated_at": True,
+                "sort_field": "name",
+                "sort_order": "asc",
+            },
+        )
     )
 
     @get(urls.PROJECT_LIST)
     async def list_project(
         self,
-        service: ProjectService,
+        project_service: ProjectService,
         filters: Annotated[list[FilterTypes], Dependency(skip_validation=True)],
-    ) -> Sequence[m.Project]:
+    ) -> OffsetPagination[Project]:
         """Get a list of Models."""
         default_filters = [
             OrderBy(field_name="created_at", sort_order="desc"),
             CollectionFilter(field_name="is_archived", values=[False]),
         ] + (filters or [])
-        return await service.list(*default_filters)
+
+        project_objs, count = await project_service.list_and_count(*default_filters)
+
+        return project_service.to_schema(data=project_objs, total=count, filters=default_filters, schema_type=Project)
 
     @post(urls.PROJECT_CREATE, guards=[requires_superuser])
     async def create_project(
         self,
-        data: m.Project,
+        data: ProjectCreate,
         current_user: m.User,
-        service: ProjectService,
-    ) -> Response[Any]:
+        project_service: ProjectService,
+        teams_service: TeamService,
+    ) -> Project:
         """Create an `Model`."""
 
         data.owner_id = current_user.id
+        team_ids = data.team_ids
+        teams = []
+        for team_id in team_ids:
+            team_obj = await teams_service.get_one_or_none(id=team_id)
+
+            if team_obj is None:
+                raise NotFoundException(detail="The Assigned Team not found", status_code=409)
+
+            teams.append(team_obj)
+
         try:
-            project = await service.create(data)
-            return Response(status_code=HTTP_200_OK, content=project.to_dict())
-        except ValueError:
-            return Response(status_code=409, content={"message": "Slug is not unique"})
+            project_obj = await project_service.create(data)
+            if len(teams) > 0:
+                project_obj.teams = teams
+            return project_service.to_schema(data=project_obj, schema_type=Project)
+        except Exception as e:
+            logger.error(e)
+            raise ValidationException(status_code=409, detail="Slug is not unique")
 
     @get(urls.PROJECT_DETAIL)
-    async def get_project(self, service: ProjectService, id: UUID) -> m.Project:
+    async def get_project(self, project_service: ProjectService, id: UUID) -> Project:
         """Get Model by ID."""
-        return await service.get(id)
+        project_obj = await project_service.get(id)
+
+        return project_service.to_schema(project_obj, schema_type=Project)
 
     @get(urls.PROJECT_DETAIL_BY_SLUG)
-    async def get_project_by_slug(self, service: ProjectService, slug: str) -> m.Project:
+    async def get_project_by_slug(self, project_service: ProjectService, slug: str) -> Project:
         """Get Model by ID."""
-        return await service.repository.get_by_slug(slug)
+        project_obj = await project_service.repository.get_by_slug(slug)
+        return project_service.to_schema(project_obj, schema_type=Project)
 
     @put(urls.PROJECT_UPDATE, guards=[requires_superuser])
     async def update_project(
         self,
-        data: m.Project,
+        data: ProjectUpdate,
         current_user: m.User,
-        service: ProjectService,
+        project_service: ProjectService,
         id: UUID,
-    ) -> "m.Project":
+    ) -> Project:
         """Update an Model."""
         data.owner_id = current_user.id
-        return await service.update(item_id=id, data=data)
+        project_obj = await project_service.update(item_id=id, data=data)
+        return project_service.to_schema(project_obj, schema_type=Project)
 
-    @patch(urls.PROJECT_STATUS_UPDATE, guards=[requires_superuser], dto=StatusWriteDTO)
+    @patch(urls.PROJECT_STATUS_UPDATE, guards=[requires_superuser])
     async def update_project_status(
         self,
-        service: ProjectService,
-        data: m.Project,
+        project_service: ProjectService,
+        data: ProjectStatusUpdate,
         id: UUID,
-    ) -> "m.Project":
+    ) -> Project:
         """Increase proejct status."""
-        project = await service.get_one_or_none(id=id)
+        project = await project_service.get_one_or_none(id=id)
         if project is None:
             raise NotFoundException("Project not found!")
 
-        return await service.update(item_id=id, data={"status": data.status})
+        project_obj = await project_service.update(item_id=id, data={"status": data.status})
+        return project_service.to_schema(project_obj, schema_type=Project)
 
     @delete(urls.PROJECT_DELETE, status_code=HTTP_200_OK, guards=[requires_superuser])
-    async def delete_project(self, service: ProjectService, id: UUID) -> None:
+    async def delete_project(self, project_service: ProjectService, id: UUID) -> None:
         """Delete Author by ID."""
-        project_obj = await service.get_one_or_none(id=id)
+        project_obj = await project_service.get_one_or_none(id=id)
         if project_obj:
-            await service.update(item_id=id, data={"is_archived": True})
+            await project_service.update(item_id=id, data={"is_archived": True})
--- src/app/domain/projects/schemas.py
+++ src/app/domain/projects/schemas.py
@@ -1,79 +1,78 @@
 from __future__ import annotations
 
 
-from datetime import date, datetime
 from typing import Any, Optional
 from uuid import UUID
-
-from pydantic import Field
+from datetime import date
 from app.db.models.enums import ProjectStatus
 from app.lib.schema import CamelizedBaseStruct
+from app.domain.teams.schemas import Team
+
+__all__ = ["ProjectCreate", "ProjectUpdate", "ProjectStatusUpdate"]
 
 
-__all__ = ["Project"]
-
-
-class Project(CamelizedBaseStruct):
+class ProjectCreate(CamelizedBaseStruct):
     """Project schemas."""
 
     slug: str
     name: str
     description: str
     pin: bool = False
-    labels: list[str] = Field(default_factory=list)
-    documents: list[str] = Field(default_factory=list)
-    start_date: date = Field(default_factory=lambda: datetime.utcnow().date())
-    end_date: date = Field(default_factory=lambda: datetime.utcnow().date())
+    labels: list[str] = []
+    documents: list[str] = []
     sprint_weeks: Optional[int] = 2
     sprint_amount: Optional[int] = 3
     sprint_checkup_day: Optional[int] = 1
-    repo_urls: list[str] = Field(default_factory=list)
+    repo_urls: list[str] = []
     plugin_meta: dict[str, Any] | None = None
-    owner: str | None = None
-    owner_id: UUID | None = None
+    owner_id: Optional[UUID] = None
     is_archived: bool = False
-    status: ProjectStatus = ProjectStatus.NOT_STARTED
+    status: Optional[ProjectStatus] = ProjectStatus.NOT_STARTED
+    team_ids: list[str] = []
 
 
-class ProjectCreate(CamelizedBaseStruct):
-    """
-    Schema used for creating a new Project.
-    Excludes:
-      - id
-      - created_at
-      - updated_at
-      - sprintlogs
-      - plugin_meta
-      - owner
-    """
-
+class Project(CamelizedBaseStruct):
     slug: str
     name: str
     description: str
+    start_date: date
+    end_date: date
     pin: bool = False
-    labels: list[str] = Field(default_factory=list)
-    documents: list[str] = Field(default_factory=list)
-    start_date: date = Field(default_factory=lambda: datetime.utcnow().date())
-    end_date: date = Field(default_factory=lambda: datetime.utcnow().date())
+    labels: list[str] = []
+    documents: list[str] = []
     sprint_weeks: Optional[int] = 2
     sprint_amount: Optional[int] = 3
     sprint_checkup_day: Optional[int] = 1
-    repo_urls: list[str] = Field(default_factory=list)
+    repo_urls: list[str] = []
+    owner_id: UUID | None = None
+    is_archived: bool = False
+    status: Optional[ProjectStatus] = ProjectStatus.NOT_STARTED
+    teams: list[Team] = []
 
 
 class ProjectUpdate(CamelizedBaseStruct):
     """Schema used when updating an existing Project."""
 
-    # You could make fields optional here as well, if partial updates are allowed
-    pass
+    id: str
+    slug: str
+    name: str
+    start_date: date
+    end_date: date
+    description: str
+    pin: bool = False
+    labels: list[str] = []
+    documents: list[str] = []
+    sprint_weeks: Optional[int] = 2
+    sprint_amount: Optional[int] = 3
+    sprint_checkup_day: Optional[int] = 1
+    repo_urls: list[str] = []
+    plugin_meta: dict[str, Any] | None = None
+    owner: str | None = None
+    owner_id: UUID | None = None
+    is_archived: bool = False
+    status: Optional[ProjectStatus] = ProjectStatus.NOT_STARTED
+    team_ids: list[str] = []
 
 
-class ProjectRead(CamelizedBaseStruct):
-    """Schema used to read a Project from the database."""
-
-    id: UUID
-    owner_id: Optional[UUID] = None
-
-    class Config:
-        orm_mode = True
-        # ensures that reading from SQLAlchemy objects via ORM is allowed
+class ProjectStatusUpdate(CamelizedBaseStruct):
+    status: ProjectStatus
